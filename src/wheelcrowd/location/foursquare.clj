19d0598223abeaf57b4a628a44b0f3a13b38bfea
Elise Huard <elise@jabberwocky.eu>
2012-11-03 19:56:49 +0000

    location with foursquare: start

--- src/wheelcrowd/location/foursquare.clj
+++ src/wheelcrowd/location/foursquare.clj
@@ -1,1 +1,16 @@
-(ns wheelcrowd.location.foursquare))
+(ns wheelcrowd.location.foursquare
+  (:require [clj-http.client :as client]))
+
+(def foursquare-config
+  {'client-id "2R5BMXPAADPLBIXDVLTP5N0BFBA3JWR4RZUKSLG5XCHIU2CQ"
+   'client-secret "XAUOLWJPRZNFR2OXYBGUI3RAKS3RIEH0HLAVEQ1WRD3MIDY4"})
+
+(defn search-request[lat lon foursquare-config]
+  (str "https://api.foursquare.com/v2/venues/search?"
+       "ll=" lat "," lon 
+       "&client_id="     (foursquare-config 'client-id) 
+       "&client_secret=" (foursquare-config 'client-secret)))
+
+(defn search[lat lon foursquare-config]
+  (let [response (client/get (search-request lat lon foursquare-config) {:as :json})]
+    (response :body)))
--- test/wheelcrowd/test/location/foursquare.clj
+++ test/wheelcrowd/test/location/foursquare.clj
@@ -1,1 +1,7 @@
-(ns wheelcrowd.test.location.foursquare))
+(ns wheelcrowd.test.location.foursquare
+  (:use [wheelcrowd.location.foursquare])
+  (:use [clojure.test]))
+
+(deftest search-request-test
+  (is (= (search-request 10.0 12.2 {'client-id "ABC" 'client-secret "DEF"})
+         "https://api.foursquare.com/v2/venues/search?ll=10.0,12.2&client_id=ABC&client_secret=DEF")))
