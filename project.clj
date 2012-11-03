a0741c7a25992c17ac1c00bfbb560bf6731c1290
Elise Huard <elise@jabberwocky.eu>
2012-11-03 19:56:23 +0000

    clojure http

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Wheelcrowd: how accessible are locations around us?"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.1.3"]
+                 [clj-http "0.5.7"]
                  [hiccup "1.0.1"]]
   :plugins [[lein-ring "0.7.5"]]
   :ring {:handler wheelcrowd.routes/app})
