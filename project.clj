a4202d955c502960f36b8cc29fd68e796ebae6d8
Elise Huard <elise@jabberwocky.eu>
2012-11-04 11:03:34 +0000

    json lib

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject wheelcrowd "1.0.0-SNAPSHOT"
   :description "Wheelcrowd: how accessible are locations around us?"
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.2.1"]
                  [compojure "1.1.3"]
                  [clj-http "0.5.7"]
                  [hiccup "1.0.1"]]
