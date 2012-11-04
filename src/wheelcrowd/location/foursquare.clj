986b310b31750052c728ed6bf4488a5a5a33f6b5
Elise Huard <elise@jabberwocky.eu>
2012-11-04 11:04:00 +0000

    handle foursquare API (TODO error handling)

--- src/wheelcrowd/location/foursquare.clj
+++ src/wheelcrowd/location/foursquare.clj
@@ -1,5 +1,6 @@
 (ns wheelcrowd.location.foursquare
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client])
+  (:require [clojure.data.json :as json]))
 
 (def foursquare-config
   {'client-id "2R5BMXPAADPLBIXDVLTP5N0BFBA3JWR4RZUKSLG5XCHIU2CQ"
@@ -11,6 +12,18 @@
        "&client_id="     (foursquare-config 'client-id) 
        "&client_secret=" (foursquare-config 'client-secret)))
 
+(defn search-call[lat lon foursquare-config]
+  (let [response (client/get (search-request lat lon foursquare-config))]
+    (json/read-str (response :body))))
+
+(defn get-items[json]
+   ((first ((json "response") "groups")) "items"))
+
+(defn relevant-details[location-json]
+  {:id   (location-json "id")
+   :name (location-json "name")
+   :categories (map (fn[x] (x "name")) (location-json "categories")) }
+)
+
 (defn search[lat lon foursquare-config]
-  (let [response (client/get (search-request lat lon foursquare-config) {:as :json})]
-    (response :body)))
+   (map relevant-details (get-items (search-call lat lon foursquare-config))))
--- test/wheelcrowd/test/location/foursquare.clj
+++ test/wheelcrowd/test/location/foursquare.clj
@@ -1,7 +1,18 @@
 (ns wheelcrowd.test.location.foursquare
   (:use [wheelcrowd.location.foursquare])
+  (:use [wheelcrowd.test.location.fixture])
   (:use [clojure.test]))
 
 (deftest search-request-test
   (is (= (search-request 10.0 12.2 {'client-id "ABC" 'client-secret "DEF"})
          "https://api.foursquare.com/v2/venues/search?ll=10.0,12.2&client_id=ABC&client_secret=DEF")))
+
+(deftest search-test
+  (with-redefs [search-call (fn[a b c] foursquare-response)]
+    (is (= (count (search 51.533599 -0.0937594 foursquare-config)) 30))))
+
+(deftest relevant-details-test
+  (let [details (relevant-details single-location)]
+    (is (= (details :id) "4e2442cdd4c0d325910996f3"))
+    (is (= (details :name) "Anussage building"))
+    (is (= (details :categories) []))))
