01d94be3aa1f3fd181df9c7acdfe6b364d06105e
Elise Huard <elise@jabberwocky.eu>
2012-11-05 17:49:26 +0000

    get tips from foursquare - check if they contain tags

--- src/wheelcrowd/location/foursquare.clj
+++ src/wheelcrowd/location/foursquare.clj
@@ -3,14 +3,17 @@
   (:require [clojure.data.json :as json]))
 
 (def config
-  {'client-id (get (System/getenv) "FOURSQUARE_CLIENT_ID")
-   'client-secret (get (System/getenv) "FOURSQUARE_CLIENT_SECRET")})
+  {:client-id (get (System/getenv) "FOURSQUARE_CLIENT_ID")
+   :client-secret (get (System/getenv) "FOURSQUARE_CLIENT_SECRET")})
+
+(defn auth-params[config]
+  (str "client_id="      (config :client-id) 
+       "&client_secret=" (config :client-secret)))
 
 (defn search-request[lat lon foursquare-config]
   (str "https://api.foursquare.com/v2/venues/search?"
-       "ll=" lat "," lon 
-       "&client_id="     (foursquare-config 'client-id) 
-       "&client_secret=" (foursquare-config 'client-secret)))
+       "ll=" lat "," lon "&"
+       (auth-params foursquare-config)))
 
 (defn search-call[lat lon foursquare-config]
   (let [response (client/get (search-request lat lon foursquare-config))]
@@ -19,7 +22,7 @@
 (defn get-items[json]
    ((first ((json "response") "groups")) "items"))
 
-(defn relevant-details[location-json]
+(defn search-relevant-details[location-json]
   {:id   (location-json "id")
    :name (location-json "name")
    :distance ((location-json "location") "distance")
@@ -27,4 +30,24 @@
 
 (defn search[lat lon foursquare-config]
   (sort-by :distance <
-    (map relevant-details (get-items (search-call lat lon foursquare-config)))))
+    (map search-relevant-details (get-items (search-call lat lon foursquare-config)))))
+
+(defn tips-request[id config]
+  (str "https://api.foursquare.com/v2/venues/" id "/tips?"
+       (auth-params config)))
+
+(defn tips-call[id config]
+  (let [response (client/get (tips-request id config))]
+    (json/read-str (response :body))))
+
+(defn tips-get-items[json]
+  (((json "response") "tips") "items"))
+
+(defn tip-accessible[tip]
+  (let [text (tip "text")]
+    (cond (re-find #"#accessfail" text) false
+          (re-find #"#accesspass" text) true
+          :else nil)))
+
+(defn tips[id config]
+   (map tip-accessible (tips-get-items (tips-call id config))))
--- test/wheelcrowd/test/location/foursquare.clj
+++ test/wheelcrowd/test/location/foursquare.clj
@@ -4,7 +4,7 @@
   (:use [clojure.test]))
 
 (deftest search-request-test
-  (is (= (search-request 10.0 12.2 {'client-id "ABC" 'client-secret "DEF"})
+  (is (= (search-request 10.0 12.2 {:client-id "ABC" :client-secret "DEF"})
          "https://api.foursquare.com/v2/venues/search?ll=10.0,12.2&client_id=ABC&client_secret=DEF")))
 
 (deftest search-test
@@ -12,8 +12,20 @@
     (is (= (count (search 51.533599 -0.0937594 config)) 30))))
 
 (deftest relevant-details-test
-  (let [details (relevant-details single-location)]
+  (let [details (search-relevant-details single-location)]
     (is (= (details :id) "4e2442cdd4c0d325910996f3"))
     (is (= (details :name) "Anussage building"))
     (is (= (details :distance) 276)) 
     (is (= (details :categories) []))))
+
+(deftest tips-request-test
+  (is (= (tips-request "ABCDEF" {:client-id "ABC" :client-secret "DEF"})
+         "https://api.foursquare.com/v2/venues/ABCDEF/tips?client_id=ABC&client_secret=DEF")))
+
+(deftest tip-accessible-test
+  (is (= (tip-accessible {"text" "great place #accesspass"}) true))
+  (is (= (tip-accessible {"text" "boo hiss #accessfail"}) false))
+  (is (= (tip-accessible {"text" "nice coffee, bad service"}) nil)))
+
+(deftest tips-test
+  (is (= (count (tips "4ace6a86f964a52072d020e3" config)) 12)))
