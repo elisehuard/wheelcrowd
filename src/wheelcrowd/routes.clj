b57c9d539d6b18daeedfb94528a6ff0de3126e3b
Elise Huard <elise@jabberwocky.eu>
2012-11-06 11:34:35 +0000

    pass in augmented info in the route

--- src/wheelcrowd/routes.clj
+++ src/wheelcrowd/routes.clj
@@ -5,12 +5,13 @@
     (:require [compojure.route :as route]
               [compojure.handler :as handler]
               [compojure.response :as response]
-              [wheelcrowd.location.foursquare :as foursquare]))
+              [wheelcrowd.location.foursquare :as foursquare]
+              [wheelcrowd.location :as location]))
 
 (defroutes main-routes
              (GET "/" [] (index-page))
              (GET "/venues" [lat lon]
-                  (venues-page (foursquare/search lat lon foursquare/config)))
+                (venues-page (location/venues lat lon foursquare/config)))
              (route/resources "/")
              (route/not-found "Page not found"))
 
