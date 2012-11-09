0ba2f9eaf93498f90d5c5a16a85c152e712dd68d
Elise Huard <elise@jabberwocky.eu>
2012-11-09 23:34:57 +0000

    database

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Wheelcrowd: how accessible are locations around us?"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.2.1"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [postgresql/postgresql "9.1-901.jdbc4"]
                  [compojure "1.1.3"]
                  [clj-http "0.5.7"]
                  [hiccup "1.0.1"]]
