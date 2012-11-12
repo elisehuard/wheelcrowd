8ab454770b001be4bd1815c3370bea9282597422
Elise Huard <elise@jabberwocky.eu>
2012-11-12 22:10:04 +0000

    add button to view

--- src/wheelcrowd/views.clj
+++ src/wheelcrowd/views.clj
@@ -35,12 +35,26 @@
 (defn venues-page [locations]
   (layout (map single-venue locations)))
 
+
+(defn accessible-button [location]
+  [:form {:action (str "/venue/" (location :id) "/accessible") :method "post"}
+    [:input {:type "hidden" :name "venue-name" :value (location :name)}]
+    [:input {:type "submit" :class "accessible" :value "accessible"}]])
+
+(defn not-accessible-button [location]
+  [:form {:action (str "/venue/" (location :id) "/not-accessible") :method "post"}
+    [:input {:type "hidden" :name "venue-name" :value (location :name)}]
+    [:input {:type "submit" :class "not-accessible" :value "not accessible"}]])
+
 (defn show-venue [location]
   [:div.location
    [:div {:data-id (location :id) }
      [:a {:href (str "/venue/" (location :id)) } [:span.name (location :name)]]
      [:span (str "Accessible: " (accessibility (location :accessible)))]
-     [:a {:href (str "https://foursquare.com/v/" (location :id)) } "Foursquare link"]]])
+     [:a {:href (str "https://foursquare.com/v/" (location :id)) } "Foursquare link"]
+     [:div
+       (accessible-button location)
+       (not-accessible-button location)]]])
 
 (defn venue-page [location]
   (layout (show-venue location))) 
