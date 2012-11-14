f9d1ec9347b81a246c1db9b7d9270d09a3e86492
Elise Huard <elise@jabberwocky.eu>
2012-11-14 10:40:05 +0000

    show connection error directly

--- src/wheelcrowd/data/migration.clj
+++ src/wheelcrowd/data/migration.clj
@@ -3,15 +3,13 @@
     (:require [clojure.java.jdbc :as sql]))
 
 (defn create-accessibility []
-  (try
-    (sql/with-connection (postgres-config)
-      (sql/create-table :rating
-        [:id :serial "PRIMARY KEY"]
-        [:foursquare_id :varchar "UNIQUE"]
-        [:name :varchar "NOT NULL"]
-        [:accessible :boolean]
-        [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))
-     (catch Exception e (.printStackTrace (.getNextException e)))))
+  (sql/with-connection (postgres-config)
+    (sql/create-table :rating
+      [:id :serial "PRIMARY KEY"]
+      [:foursquare_id :varchar "UNIQUE"]
+      [:name :varchar "NOT NULL"]
+      [:accessible :boolean]
+      [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])))
 
 (defn -main []
     (print "Migrating database...") (flush)
