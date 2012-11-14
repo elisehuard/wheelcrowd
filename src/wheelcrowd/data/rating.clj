ebef4f617d65ae1098569cec77a267cd7127851c
Elise Huard <elise@jabberwocky.eu>
2012-11-14 11:12:47 +0000

    simplified postgres access again for rating

--- src/wheelcrowd/data/rating.clj
+++ src/wheelcrowd/data/rating.clj
@@ -3,20 +3,20 @@
   (require [clojure.java.jdbc :as sql]))
 
 (defn get-rating [foursquare-id]
-  (sql/with-connection (postgres-config)
+  (sql/with-connection postgres-db
     (sql/with-query-results results
       [(str "select * from rating where foursquare_id = '" foursquare-id "';")]
       (into [] results))))
 
 (defn new-rating[data]
-  (sql/with-connection (postgres-config)
+  (sql/with-connection postgres-db
     (sql/insert-values :rating [:foursquare_id :name :accessible]
                                [(data :foursquare-id) (data :name) (data :accessible)])))
 
 (defn update-rating
   "This method updates a rating"
   [id attribute-map]
-  (sql/with-connection (postgres-config)
+  (sql/with-connection postgres-db
     (sql/update-values
       :rating
         ["foursquare_id=?" id]
